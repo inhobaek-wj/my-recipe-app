59f50ce17d232e47b78944f6784978e71e3b8299
inhobaek-wj <inhobaek.wj@gmail.com>
2019-12-13 00:02:07 +0900

    add thumnail

--- pages/recipes/index.vue
+++ pages/recipes/index.vue
@@ -1,17 +1,19 @@
 <template>
   <section class="recipes">
 
-    <airticle class="recipe">
-      <div></div>
+    <article class="recipe">
+      <div class="thumnail"
+           style="background-image: url('https://s3.ap-northeast-2.amazonaws.com/img.kormedi.com/news/article/__icsFiles/artimage/2016/03/29/c_km601/911811_540.jpg')"></div>
       <h1>Title</h1>
       <p>Some nice preview text</p>
-    </airticle>
+    </article>
 
-    <airticle class="recipe">
-      <div></div>
+    <article class="recipe">
+      <div class="thumnail"
+           style="background-image: url('https://img1.daumcdn.net/thumb/R720x0/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fliveboard%2Frealfood%2Fd3d9b8fce02048bfb5564fd1b344f1cb.jpg')"></div>
       <h1>Title</h1>
       <p>Some nice preview text</p>
-    </airticle>
+    </article>
 
   </section>
 </template>
@@ -28,5 +30,12 @@
   padding: 8px;
   border: 1px solid #ccc;
   box-shadow: 0 2px 2px #aaa;
+}
+
+.thumnail {
+  background-position: center;
+  background-size:cover;
+  width: 100%;
+  height: 200px;
 }
 </style>
