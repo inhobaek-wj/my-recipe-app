197b2874466fe5b646f7d2d9d94988a75b054f6b
inhobaek-wj <inhobaek.wj@gmail.com>
2019-12-13 01:40:07 +0900

    style

--- components/Recipe.vue
+++ components/Recipe.vue
@@ -15,7 +15,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 a {
   text-decoration: none;
   color: black;
--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,3 +1,9 @@
 <template>
   <h1>The ABOUT page</h1>
 </template>
+
+<style>
+h1 {
+  text-align: center;
+}
+</style>
