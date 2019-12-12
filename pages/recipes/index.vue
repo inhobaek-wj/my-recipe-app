0a17e66f7f4937059b1fd3f952a3cbb453b5f0e9
inhobaek-wj <inhobaek.wj@gmail.com>
2019-12-13 01:00:18 +0900

    use asyncData() for fake request & v-for directive

--- pages/recipes/index.vue
+++ pages/recipes/index.vue
@@ -1,16 +1,12 @@
 <template>
   <section class="recipes">
     <Recipe
-      previewText="Awesome chicken"
-      title="Delicious Chicken"
-      id="1"
-      thumbnail="https://s3.ap-northeast-2.amazonaws.com/img.kormedi.com/news/article/__icsFiles/artimage/2016/03/29/c_km601/911811_540.jpg" />
-
-    <Recipe
-      previewText="Delicious ramen"
-      title="ramen"
-      id="2"
-      thumbnail="https://img1.daumcdn.net/thumb/R720x0/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fliveboard%2Frealfood%2Fd3d9b8fce02048bfb5564fd1b344f1cb.jpg" />
+      v-for="recipe in recipes"
+      :key="recipe.id"
+      :previewText="recipe.previewText"
+      :title="recipe.title"
+      :id="recipe.id"
+      :thumbnail="recipe.thumbnail" />
 
   </section>
 </template>
@@ -21,6 +17,30 @@ import Recipe from '@/components/Recipe'
 export default {
   components: {
     Recipe
+  },
+
+  // this allows server side rendering.
+  asyncData() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          recipes: [
+            {
+              id: 1,
+              title: "Delicious Chicken",
+              previewText: "Awesome chicken",
+              thumbnail: "https://s3.ap-northeast-2.amazonaws.com/img.kormedi.com/news/article/__icsFiles/artimage/2016/03/29/c_km601/911811_540.jpg"
+            },
+            {
+              id: 2,
+              title: "Ramen",
+              previewText: "Awesome chicken",
+              thumbnail: "https://img1.daumcdn.net/thumb/R720x0/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fliveboard%2Frealfood%2Fd3d9b8fce02048bfb5564fd1b344f1cb.jpg"
+            }
+          ]
+        })
+      }, 1500)
+    })
   }
 }
 
